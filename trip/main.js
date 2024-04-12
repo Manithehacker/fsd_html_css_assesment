62519d243a54a979ca194a5ad75548ed183005be
Manithehacker <mkmani176@gmail.com>
2024-04-12 12:09:14 +0530

    12/04/2024

--- trip/main.js
+++ trip/main.js
@@ -8,52 +8,73 @@ const individual = () => {
   let form = document.createElement("form");
   form.id = "form1";
 
+  let lab1=document.createElement("label")
+  lab1.innerHTML="Name"
+
   let input1 = document.createElement("input");
   input1.placeholder = "Enter your name";
   input1.id = "inp1";
 
+  let lab2=document.createElement("label")
+  lab2.innerHTML="Email ID"
+  
   let input2 = document.createElement("input");
   input2.placeholder = "Enter your email";
   input2.id = "inp2";
+
+  let lab3=document.createElement("label")
+  lab3.innerHTML="Mobile"
 
   let input3 = document.createElement("input");
   input3.placeholder = "Enter your mobile";
   input3.id = "inp3";
 
+  let lab4=document.createElement("label")
+  lab4.innerHTML="Location"
+
   let inpFrom = document.createElement("input");
   inpFrom.placeholder = "Enter your location";
   inpFrom.id = "inpf";
 
+  let lab5=document.createElement("label")
+  lab5.innerHTML="Destination"
   let inpTo = document.createElement("input");
   inpTo.placeholder = "Enter the Destination";
   inpTo.id = "inpto";
 
+  let lab6=document.createElement("label")
+  lab6.innerHTML="Date"
   let input4 = document.createElement("input");
   input4.type = "date";
   input4.setAttribute("id", "inp4");
 
+ 
   let submit = document.createElement("input");
   submit.type = "submit";
   submit.id = "submit1";
   submit.onclick = setData;
 
   //select tag creation
-  var sel = document.createElement("select");
-  sel.name = "drop1";
+  let lab7=document.createElement("label")
+  lab7.innerHTML="Bike"
+  var sel = document.createElement("input");
+  sel.placeholder="Bike Number"
   sel.id = "Select1";
-  var bike = ["", "mountain bike", "Hybrid nike", "road bike"];
-  var options_str = "";
-  bike.forEach((bikes) => {
-    options_str += `<option value=  ${bikes}  >  ${bikes}  </option>`;
-  });
-  sel.innerHTML = options_str;
+ 
 
+  form.appendChild(lab1);
   form.appendChild(input1);
+  form.appendChild(lab2);
   form.appendChild(input2);
+  form.appendChild(lab3);
   form.appendChild(input3);
+  form.appendChild(lab4)
   form.appendChild(inpFrom);
+  form.appendChild(lab5)
   form.appendChild(inpTo);
+  form.appendChild(lab6)
   form.appendChild(input4);
+  form.appendChild(lab7)
   form.appendChild(sel);
   form.appendChild(submit);
   document.body.appendChild(form);
@@ -62,7 +83,7 @@ const individual = () => {
 //submit data
 const setData = (e) => {
   e.preventDefault();
-
+  
   let val1 = document.getElementById("inp1").value;
   let val2 = document.getElementById("inp2").value;
   let val3 = document.getElementById("inp3").value;
@@ -84,7 +105,16 @@ const setData = (e) => {
 
   //converting objects into json format then store into localstorage
  localStorage.setItem("solo", JSON.stringify(obj))
-  pop()
+
+ var d = new Date().toISOString().split('T')[0];
+
+ if(d==val4){
+  alert("processing")
+ }
+ else{
+ alert("waiting")
+ }
+
     //once the data submited successfully display block setting the  main container 
     document.getElementById("main").style.display = "block";
     alert("Form submitted successfully");
@@ -94,25 +124,22 @@ const setData = (e) => {
 };
 
 //delete data
-const deleteData = () => {
-  localStorage.clear();
-  location.reload();
-};
+
 
 //get data
 const getData = () => {
   document.getElementById("main").style.display = "none";
   // getting values from localstoarge
-  let data = localStorage.getItem("soloride");
+  // let data = localStorage.getItem("soloride");
   let data2 = localStorage.getItem("solo");
   try {
-    if (data || data2) {
+    if ( data2) {
       //parsing data
-      let da = JSON.parse(data);
+      // let da = JSON.parse(data);
       let da2 = JSON.parse(data2);
 
       //storing data in array
-      let arr = [da, da2];
+      let arr = [da2];
 
       //declaring the varaible
       let user;
@@ -149,7 +176,7 @@ const getData = () => {
     <p id="card-bike"> ${bike}</p>
     
     <div id="icon">
-    <i class="fa-solid fa-pencil" onclick=test()></i>
+    <i class="fa-solid fa-pencil" onclick=editData()></i>
     <i class="fa-solid fa-trash" onclick=deleteData()></i>
     </div>
     `;
@@ -167,3 +194,14 @@ const getData = () => {
 };
 
 //deleteData
+const deleteData = () => {
+  localStorage.removeItem("solo");
+  alert("Deleted successfully")
+  location.reload();
+
+};
+
+//editData
+const editData=()=>{
+  console.log("")
+}
